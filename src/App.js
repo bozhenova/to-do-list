63e5af710569a2d858eab6d3abb5703b271f4f8c
Margo <margo.bozhenova@gmail.com>
2020-03-01 19:24:26 +0300

    Add TodoList,TodoItem and type validation

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, completed: false, title: 'Купить хлеб' },
+    { id: 2, completed: false, title: 'Купить масло' },
+    { id: 3, completed: false, title: 'Купить молоко' }
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos!</p>}
+
+      </div>
+    </Context.Provider>
+  )
+}
+
+export default App;
