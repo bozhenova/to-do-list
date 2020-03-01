89aa1e748b18891950e7801278014ebed1f9c18d
Margo <margo.bozhenova@gmail.com>
2020-03-02 01:55:03 +0300

    Add AddTodo component and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context';
+import Loader from './Loader';
+
+const AddTodo = React.lazy(() =>
+  import('./Todo/AddTodo')
+);
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, completed: false, title: 'Купить хлеб' },
-    { id: 2, completed: false, title: 'Купить масло' },
-    { id: 3, completed: false, title: 'Купить молоко' }
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000)
+      })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(todos.map(todo => {
@@ -22,12 +35,27 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id));
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title: title,
+        id: Date.now,
+        completed: false
+      }
+    ]))
+  }
+
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutorial</h1>
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos!</p>}
-
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+        {loading && <Loader />}
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo} />) :
+          (loading ? null : <p>No todos!</p>
+          )}
       </div>
     </Context.Provider>
   )
